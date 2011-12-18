e50695eebf1ef3d50db07cd7252c68c4473d3fac
Artem Kramarenko <me@artemk.name>
2011-12-18 14:15:51 +0200

    name collision #90 #16 #117

--- lib/acts-as-taggable-on.rb
+++ lib/acts-as-taggable-on.rb
@@ -11,15 +11,14 @@ require "acts_as_taggable_on/compatibility/active_record_backports" unless RAILS
 
 require "acts_as_taggable_on/utils"
 
-require "acts_as_taggable_on/acts_as_taggable_on"
+require "acts_as_taggable_on/taggable"
 require "acts_as_taggable_on/acts_as_taggable_on/core"
 require "acts_as_taggable_on/acts_as_taggable_on/collection"
 require "acts_as_taggable_on/acts_as_taggable_on/cache"
 require "acts_as_taggable_on/acts_as_taggable_on/ownership"
 require "acts_as_taggable_on/acts_as_taggable_on/related"
 
-#require "acts_as_taggable_on/utils"
-require "acts_as_taggable_on/acts_as_tagger"
+require "acts_as_taggable_on/tagger"
 require "acts_as_taggable_on/tag"
 require "acts_as_taggable_on/tag_list"
 require "acts_as_taggable_on/tags_helper"
